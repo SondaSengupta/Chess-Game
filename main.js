b0f5257694e0fe6f1271d97eed73b7d080c8a281
gregkorte <gryphionsroost@yahoo.com>
2014-11-06 09:15:17 -0600

    Highlight semi-working, selected, highlighted properties working.

--- main.js
+++ main.js
@@ -1,52 +1,103 @@
-var test = [["\u2659", "\u2659", "\u2659", "\u2659"],
-            ['', '', '', ''],
-            ['', '', '', ''],
-            ["\u265F", "\u265F", "\u265F", "\u265F"]];
-// at https://github.com/jspencersharpe/Chess-Game.git
+var board = [
+[{symbol: '\u2656', selected: 0, highlighted: 0, color: 'white', type: 'rook'}, 
+{symbol: '\u2658', selected: 0, highlighted: 0, color: 'white', type: 'knight'},
+{symbol: '\u2657', selected: 0, highlighted: 0, color: 'white', type: 'bishop'},
+{symbol: '\u2655', selected: 0, highlighted: 0, color: 'white', type: 'queen'},
+{symbol: '\u2654', selected: 0, highlighted: 0, color: 'white', type: 'king'},
+{symbol: '\u2657', selected: 0, highlighted: 0, color: 'white', type: 'bishop'},
+{symbol: '\u2658', selected: 0, highlighted: 0, color: 'white', type: 'knight'},
+{symbol: '\u2656', selected: 0, highlighted: 0, color: 'white', type: 'rook'}],
+[{symbol: '\u2659', selected: 0, highlighted: 0, color: 'white', type: 'pawn'},
+{symbol: '\u2659', selected: 0, highlighted: 0, color: 'white', type: 'pawn'},
+{symbol: '\u2659', selected: 0, highlighted: 0, color: 'white', type: 'pawn'},
+{symbol: '\u2659', selected: 0, highlighted: 0, color: 'white', type: 'pawn'},
+{symbol: '\u2659', selected: 0, highlighted: 0, color: 'white', type: 'pawn'},
+{symbol: '\u2659', selected: 0, highlighted: 0, color: 'white', type: 'pawn'},
+{symbol: '\u2659', selected: 0, highlighted: 0, color: 'white', type: 'pawn'},
+{symbol: '\u2659', selected: 0, highlighted: 0, color: 'white', type: 'pawn'}],
+[{symbol: '', selected: 0, highlighted: 0, color: '', type: ''},
+{symbol: '', selected: 0, highlighted: 0, color: '', type: ''},
+{symbol: '', selected: 0, highlighted: 0, color: '', type: ''},
+{symbol: '', selected: 0, highlighted: 0, color: '', type: ''},
+{symbol: '', selected: 0, highlighted: 0, color: '', type: ''},
+{symbol: '', selected: 0, highlighted: 0, color: '', type: ''},
+{symbol: '', selected: 0, highlighted: 0, color: '', type: ''},
+{symbol: '', selected: 0, highlighted: 0, color: '', type: ''}],
+[{symbol: '', selected: 0, highlighted: 0, color: '', type: ''},
+{symbol: '', selected: 0, highlighted: 0, color: '', type: ''},
+{symbol: '', selected: 0, highlighted: 0, color: '', type: ''},
+{symbol: '', selected: 0, highlighted: 0, color: '', type: ''},
+{symbol: '', selected: 0, highlighted: 0, color: '', type: ''},
+{symbol: '', selected: 0, highlighted: 0, color: '', type: ''},
+{symbol: '', selected: 0, highlighted: 0, color: '', type: ''},
+{symbol: '', selected: 0, highlighted: 0, color: '', type: ''}],
+[{symbol: '', selected: 0, highlighted: 0, color: '', type: ''},
+{symbol: '', selected: 0, highlighted: 0, color: '', type: ''},
+{symbol: '', selected: 0, highlighted: 0, color: '', type: ''},
+{symbol: '', selected: 0, highlighted: 0, color: '', type: ''},
+{symbol: '', selected: 0, highlighted: 0, color: '', type: ''},
+{symbol: '', selected: 0, highlighted: 0, color: '', type: ''},
+{symbol: '', selected: 0, highlighted: 0, color: '', type: ''},
+{symbol: '', selected: 0, highlighted: 0, color: '', type: ''}],
+[{symbol: '', selected: 0, highlighted: 0, color: '', type: ''},
+{symbol: '', selected: 0, highlighted: 0, color: '', type: ''},
+{symbol: '', selected: 0, highlighted: 0, color: '', type: ''},
+{symbol: '', selected: 0, highlighted: 0, color: '', type: ''},
+{symbol: '', selected: 0, highlighted: 0, color: '', type: ''},
+{symbol: '', selected: 0, highlighted: 0, color: '', type: ''},
+{symbol: '', selected: 0, highlighted: 0, color: '', type: ''},
+{symbol: '', selected: 0, highlighted: 0, color: '', type: ''}],
+[{symbol: '\u265F', selected: 0, highlighted: 0, color: 'black', type: 'pawn'},
+{symbol: '\u265F', selected: 0, highlighted: 0, color: 'black', type: 'pawn'},
+{symbol: '\u265F', selected: 0, highlighted: 0, color: 'black', type: 'pawn'},
+{symbol: '\u265F', selected: 0, highlighted: 0, color: 'black', type: 'pawn'},
+{symbol: '\u265F', selected: 0, highlighted: 0, color: 'black', type: 'pawn'},
+{symbol: '\u265F', selected: 0, highlighted: 0, color: 'black', type: 'pawn'},
+{symbol: '\u265F', selected: 0, highlighted: 0, color: 'black', type: 'pawn'},
+{symbol: '\u265F', selected: 0, highlighted: 0, color: 'black', type: 'pawn'}],
+[{symbol: '\u265C', selected: 0, highlighted: 0, color: 'black', type: 'rook'},
+{symbol: '\u265E', selected: 0, highlighted: 0, color: 'black', type: 'knight'},
+{symbol: '\u265D', selected: 0, highlighted: 0, color: 'black', type: 'bishop'},
+{symbol: '\u265B', selected: 0, highlighted: 0, color: 'black', type: 'king'},
+{symbol: '\u265A', selected: 0, highlighted: 0, color: 'black', type: 'queen'},
+{symbol: '\u265D', selected: 0, highlighted: 0, color: 'black', type: 'bishop'},
+{symbol: '\u265E', selected: 0, highlighted: 0, color: 'black', type: 'knight'},
+{symbol: '\u265C', selected: 0, highlighted: 0, color: 'black', type: 'rook'}]];
+
 $(document).ready(function(){
-  //The double array of the board
-
-  
-  //Gives each piece it's own variable
-  // var wp1 = test[0][0];
-  // var wp2 = test[1][0];
-  // var wp3 = test[2][0];
-  // var wp4 = test[3][0];
-
-  // var bp1 = test[3][0];
-  // var bp2 = test[3][1];
-  // var bp3 = test[3][2];
-  // var bp4 = test[3][3];
    
   //Function generates the board with initial state of all the pieces          
-  generateGrid(test);
+  generateGrid(board);
 
   //When piece is clicked, function determines which piece is selected
-  currentBoardState(4, 4);
-  $('td').click(function(){
+  $('body').on('click', 'td', function(){
     $('td').removeClass("selectedPiece");
-    $(this).text();
     $(this).addClass("selectedPiece");
     var columnPosition = $(this).nextAll('td');
-    var y = 3 - columnPosition.length
+    var y = 7 - columnPosition.length;
     var rowPosition = $(this).parent('tr').nextAll('tr');
-    var x = 3 - rowPosition.length
+    var x = 7 - rowPosition.length;
     // console.log(x + ', ' + y);
-    // console.log(test[x]);
-    // console.log(test[x][y]);
-
-    possibleMoves = rules(x, y);
+    // console.log(board[x][y]);
+    // console.log(board[xPos][yPos]);
+    rules(x, y);
+    if(board[xPos][yPos].highlighted === 1) {
+      $(this).addClass("possibleMoves");
+    }
+      else {$('td').removeClass("possibleMoves");
+      } 
+    // console.log(board);
   })
 }); //end of DOMContentLoaded    
 
-function generateGrid(test){
+function generateGrid(board){
   var $table = document.querySelector('#board');
 $table.innerHTML = '';
-  test.forEach(function(row){
+  board.forEach(function(row){
     var $tr = document.createElement('tr');
     row.forEach(function(cell){
       var $td = createTableCell(cell);
-      $td.textContent = cell;
+      $td.textContent = cell.symbol;
       $tr.appendChild($td);
     });
     $table.appendChild($tr);
@@ -58,51 +109,18 @@ function createTableCell(value){
   return $td;
 }
 
-//Loads currentBoardState() into memory
-function currentBoardState(x, y){
-  var test = [];
-  for(var i = 0; i < x; i++){
-    test[i] = [];
-    for(var j = 0; j < y; j++){
-      test[i][j] = test}
-  }
-return test;
-}
-
 function rules(x, y){
-  var currentPiecePosition = test[x][y];
-  if (currentPiecePosition === "\u2659"){
-    possibleMoves = [{x:x+1, y:y}];
-  //highlight (possibleMoves)
-  //If counter of pawns variable === 0
-    //Then highlight (test[x + 1][y]) && (test[x + 2][y])
-    console.log(possibleMoves[0].x, possibleMoves[0].y);
-    highlightPossibleMoves(possibleMoves[0].x, possibleMoves[0].y);
+  // console.log(board[x][y]);
+  var selectedPiece = board[x][y];
+  selectedPiece.selected = 1;
+  // console.log(selectedPiece);
+  if (selectedPiece.symbol === '\u2659'){
+    xPos = x + 1;
+    yPos = y;
+    board[xPos][yPos].highlighted = 1;
+    console.log(xPos + ', ' + yPos);
+    // console.log(possibleMoves);
+    // console.log(board);
   }
+  return board;
 }
-
-function highlightPossibleMoves() {
-  var x = possibleMoves[0].x;
-  var y = possibleMoves[0].y;
-  console.log(x, y);
-  $('td').removeClass("possibleMoves");
-  possibleMoves.forEach(function(highlight){
-  $('td').addClass("possibleMoves");
-  });
-}
-
-/*Implement into moving of pieces.
- var clicks = 0;
-    function onClick() {
-        clicks += 1;
-        document.getElementById("clicks").innerHTML = clicks;
-    };
-
-//////// Capturing Pieces
-
-// if statement about white pieces interfering with black pieces
-/
-var whitePieces = [wPawn1, wPawn2, wPawn3, wPawn4];
-  var whiteCaptured = [];
-  whiteCaptured = pieces.slice(1, 2)
-        // whiteCaptured contains [wPawn2]
